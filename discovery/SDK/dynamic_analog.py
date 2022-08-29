9e26f70837a020beb70b6b4dee8a7091ce5d329f
applevitya <38158336+applevitya@users.noreply.github.com>
2022-08-29 15:58:05 +0300

    new

--- discovery/SDK/dynamic_analog.py
+++ discovery/SDK/dynamic_analog.py
@@ -17,10 +17,31 @@ else:
 hzSys = c_double()
 """-----------------------------------------------------------------------"""
 
-
-
 #Analog to digital
 
+def measure(device_handle,channel):
+     """
+        measure a voltage
+        parameters: - device data
+                    - the selected oscilloscope channel (1-2, or 1-4)
+ 
+        returns:    - the measured voltage in Volts
+    """"
+    
+    # set up the instrument
+    dwf.FDwfAnalogInConfigure(device_data.handle, c_bool(False), c_bool(False))
+ 
+    # read data to an internal buffer
+    dwf.FDwfAnalogInStatus(device_data.handle, c_bool(False), c_int(0))
+ 
+    # extract data from that buffer
+    voltage = c_double()   # variable to store the measured voltage
+    dwf.FDwfAnalogInStatusSample(device_data.handle, c_int(channel - 1), byref(voltage))
+ 
+    # store the result as float
+    voltage = voltage.value
+    return voltage
+    
 
 
 

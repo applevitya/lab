31233e8998c48029c90a4aca59fa9e274232bf86
applevitya <38158336+applevitya@users.noreply.github.com>
2022-10-26 23:03:05 +0300

    new

--- discovery/new.py
+++ discovery/new.py
@@ -1,18 +1,4 @@
 import time
-import dwf
 
 
-t_start = time.process_time()
 
-print('ggg')
-
-data1 = [1 for i in range(64)]
-data2 = [0 for i in range(64)]
-
-datanew=[0 for i in range(64)]
-for i in range(64):
-    if i%2==0: datanew[i] = 1
-    else: datanew[i] = 0
-time.sleep(1)
-
-print(time.process_time() - t_start)

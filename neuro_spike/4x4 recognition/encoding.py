12d64bfaba974ab0272ded79c6b20a9de73ef21e
applevitya <38158336+applevitya@users.noreply.github.com>
2023-02-21 16:02:59 +0300

    new

---
+++ neuro_spike/4x4 recognition/encoding.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def Poisson_generator(time,dt,rate, n, myseed=False):
+  """
+  Generates poisson trains
+
+  Args:
+    rate            : noise amplitute [Hz]
+    n               : number of Poisson trains
+    myseed          : random seed. int or boolean
+
+  Returns:
+    pre_spike_train : spike train matrix, ith row represents whether
+                      there is a spike in ith spike train over time
+                      (1 if spike, 0 otherwise)
+  """
+
+  # Retrieve simulation parameters
+  range_t = np.arange(0, time+dt, dt)  # Vector of discretized time points [ms]
+  Lt = range_t.size
+
+  # set random seed
+  if myseed:
+      np.random.seed(seed=myseed)
+  else:
+      np.random.seed()
+
+  # generate uniformly distributed random variables
+  u_rand = np.random.rand(n, Lt)
+
+  # generate Poisson train
+  poisson_train = 1. * (u_rand < rate * (dt /300))
+
+  return poisson_train
+
---
+++ neuro_spike/4x4 recognition/parameters.py
@@ -0,0 +1,19 @@
+### Parameters of the given model
+
+
+# initial learning par
+num_in_neu = 16  # 4x4 size image
+epochs = 1
+
+num_out_neu = 2
+
+
+#  neuron par
+R = 1
+C = 1
+
+refrac_time = 0.15
+init_refrac = 0
+noise = 0
+
+thresh = 0.001

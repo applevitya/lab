6537f22eb3f0d22f287a38dd296eb4d620b183ca
applevitya <38158336+applevitya@users.noreply.github.com>
2023-03-04 19:40:33 +0300

    new

--- neuro_spike/4x4 recognition/learning.py
+++ neuro_spike/4x4 recognition/learning.py
@@ -1,5 +1,5 @@
 from neuron import LIF_simple
-from parameters import num_in_neu,num_out_neu, range_stdp
+from parameters import num_in_neu,num_out_neu, range_stdp,epochs
 from encoding import Poisson_generator, read_img
 from STDP import update
 import numpy as np
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 
 ############ Random Weights #####################
 
-W = np.random.uniform(0,0.01,size = [num_in_neu,num_out_neu])
+W = np.random.uniform(0,0.1,size = [num_in_neu,num_out_neu])
+#W = np.ones((num_in_neu,num_out_neu))*0.01
 
 #################################
 # time series
@@ -20,66 +21,73 @@ dt = 0.1
 time = np.arange(0,T+dt,dt)
 
 
-### input/output spikes #########################
-in_spikes = np.empty(shape=(num_in_neu,len(time)))
-out_spikes = np.zeros(shape=(num_out_neu,len(time)))
 
-# creating the output layer of neurons
-out_neurons = []
-for i in range(num_out_neu):
-	a = LIF_simple()
-	out_neurons.append(a)
 
 
 ##### learning ########
 
-for m in range(2,3):
-	img = read_img(str(m)+".png")
-	for l in range(num_in_neu):
-		in_spikes[l] = Poisson_generator(T, dt, 10 + img[l], 1)
+for ep in range(epochs):
 
-	I = np.zeros(shape=(num_in_neu,))
-	for t in range(len(time)):
+	### input/output spikes #########################
+	in_spikes = np.empty(shape=(num_in_neu, len(time)))
+	out_spikes = np.zeros(shape=(num_out_neu, len(time)))
 
-		for j,neu in enumerate(out_neurons):
-			I[j] = 0
-			for i in range(num_in_neu):
-				#I[j] = 0
-				I[j] += np.dot(W[i][j],in_spikes[i][t])
+	# creating the output layer of neurons
+	out_neurons = []
+	for i in range(num_out_neu):
+		a = LIF_simple()
+		out_neurons.append(a)
+
+	for m in range(1,2):
+		img = read_img(str(m)+".png")
+		for l in range(num_in_neu):
+			in_spikes[l] = Poisson_generator(T, dt, 4 + img[l], 1)
+
+		I = np.zeros(shape=(num_out_neu,))
+		for t in range(len(time)):
+
+			for j,neu in enumerate(out_neurons):
+				I[j] = 0
+				for i in range(num_in_neu):
+					#I[j] = 0
+					I[j] += np.dot(W[i][j],in_spikes[i][t])
 
 
-				if t>=neu.initRefrac:
+					if t>=neu.initRefrac:
 
-					v = neu.vprev + (-neu.vprev + I[j] * neu.R) / neu.tau_m * dt  # LIF
-					# v= neu.vprev + np.dot(W[i][j],in_spikes[i][t])
-					if (v > neu.v_base):
-						# v -= 0.01
-						if v < neu.v_base:
+						v = neu.vprev + (-neu.vprev + I[j] * neu.R) / neu.tau_m * dt  # LIF
+						# v= neu.vprev + np.dot(W[i][j],in_spikes[i][t])
+						if (v > neu.v_base):
+							# v -= 0.01
+							if v < neu.v_base:
+								v = neu.v_base
+
+						if v >= neu.v_thresh:
+							neu.num += 1
+							out_spikes[j][t] = 1
+							neu.initRefrac = t + neu.refracTime
 							v = neu.v_base
+							neu.v_thresh += 0.0012
+							neu.vprev = v
+							break
 
-					if v >= neu.v_thresh:
-						neu.num += 1
-						out_spikes[j][t] = 1
-						neu.initRefrac = t + neu.refracTime
-						v = neu.v_base
-						neu.v_thresh += 0.01
 						neu.vprev = v
-						break
 
-					neu.vprev = v
+				for i in range(num_in_neu):
+					for t1 in range(-1, -range_stdp, -1):
+						if 0 <= t + t1 < len(time):
+							if in_spikes[i][t + t1] == 1 and out_spikes[j][t] == 1:
+								W[i][j] = update(W[i][j], t1)
+					for t1 in range(1, range_stdp, 1):
+						if 0 <= t + t1 < len(time):
+							if in_spikes[i][t + t1] == 1 and out_spikes[j][t] == 1:
+								W[i][j] = update(W[i][j], t1)
+			for j, neu in enumerate(out_neurons):
+				if out_spikes[j][t] != 1:
+					neu.vprev = 0.00
 
-			for i in range(num_in_neu):
-				for t1 in range(-1, -range_stdp, -1):
-					if 0 <= t + t1 < len(time):
-						if in_spikes[i][t + t1] == 1 and out_spikes[j][t] == 1:
-							W[i][j] = update(W[i][j], t1)
-				for t1 in range(1, range_stdp, 1):
-					if 0 <= t + t1 < len(time):
-						if in_spikes[i][t + t1] == 1 and out_spikes[j][t] == 1:
-							W[i][j] = update(W[i][j], t1)
-		for j, neu in enumerate(out_neurons):
-			if out_spikes[j][t] != 1:
-				neu.vprev -= 0.01
+
+
 
 
 ######### testing ################
--- neuro_spike/4x4 recognition/parameters.py
+++ neuro_spike/4x4 recognition/parameters.py
@@ -5,27 +5,26 @@
 num_in_neu = 16  # 4x4 size image
 epochs = 1
 
-num_out_neu = 2
+num_out_neu =4
 
 
 #  neuron par
-R = 10
-C = 0.05
+R = 100
+C = 1
 
-refrac_time = 0.11
+refrac_time = 2
 init_refrac = 0
 noise = 0
 
-thresh = 0.15
+thresh = 0.1
 
 # encoding
-firing_delimeter =300
-
+firing_delimeter = 500
 # STDP
-A_plus = 0.2
-A_minus = 0.3
+A_plus = 0.9
+A_minus = 1.6
 
-tau_minus = 10
-tau_plus = 3
+tau_plus = 0.5
+tau_minus = 3
 
-range_stdp = 40
+range_stdp = 20

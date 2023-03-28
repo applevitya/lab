ef572a2e765858aa4c96e8de9fcd86fafeb98310
applevitya <38158336+applevitya@users.noreply.github.com>
2023-03-28 13:35:44 +0300

    new

--- discovery/plot.py
+++ discovery/plot.py
@@ -4,12 +4,12 @@ import sys
 import numpy as np
 import tkinter as tk
 from tkinter import ttk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-import threading
-import time
 import random
+import time
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+from matplotlib.figure import Figure
 
 if sys.platform.startswith("win"):
     dwf = cdll.dwf
@@ -21,73 +21,88 @@ else:
 hdwf = c_int()
 hdwf = device.open()
 
-
 led_off = [0 for i in range(64)]
 led_on = [1 for i in range(64)]
-dynamic_digital.led_matrix(hdwf,1,2,0,led_off) # выключаем светодиоды
+dynamic_digital.led_matrix(hdwf, 1, 2, 0, led_off) # выключаем светодиоды
 
 struct_index = [0 for i in range(64)]
 struct_index[6] = 1
-dynamic_analog.struct_measure(hdwf,4,5,6,struct_index) # struct_measure(device_handle,shift,clock,data,matrix)
+dynamic_analog.struct_measure(hdwf, 4, 5, 6, struct_index) # struct_measure(device_handle,shift,clock,data,matrix)
 
 def measure():
-    # Здесь ваша функция measure, которая возвращает число
     return dynamic_analog.measure(hdwf,1)
 
+def some_function():
+    print("Выполняется функция, не связанная с графиком")
 
-def on_button_click():
-    dynamic_digital.led_matrix(hdwf, 1, 2, 0, led_on)
-    time.sleep(0.05)
-    dynamic_digital.led_matrix(hdwf, 1, 2, 0, led_off)
-    pass
+def close():
+    print("Закрытие программы")
 
+def update_graphs():
+    global x, y1, y2,  y3 ,y4, counter
 
-def close(root):
-    dwf.FDwfDigitalOutReset(hdwf)
-    device.close(hdwf)
-    root.quit()
-    root.destroy()
+    if not stop_graph:
+        x.append(counter)
+        y1.append(measure())
+        y2.append(measure())
+        y3.append(measure())
+        y4.append(measure())
+        counter += 1
 
+        lines1.set_data(x, y1)
+        lines2.set_data(x, y2)
+        lines3.set_data(x, y3)
+        lines4.set_data(x, y4)
 
-def update_plot(plot, canvas):
-    while True:
-        data = measure()
-        plot.set_ydata(np.append(plot.get_ydata()[1:], data))
+        ax1.relim()
+        ax1.autoscale_view()
+        ax2.relim()
+        ax2.autoscale_view()
+        ax3.relim()
+        ax3.autoscale_view()
+        ax4.relim()
+        ax4.autoscale_view()
+
         canvas.draw()
-        time.sleep(0.0005)
+
+        root.after(50, update_graphs)
+
+def stop():
+    global stop_graph
+    stop_graph = True
+    close()
+    root.quit()
+
+root = tk.Tk()
+root.title("Tkinter и Matplotlib")
+root.geometry("1000x600")
+
+frame_buttons = ttk.Frame(root)
+frame_buttons.pack(side=tk.TOP, pady=20)
+
+btn_some_function = ttk.Button(frame_buttons, text="Выполнить функцию", command=some_function)
+btn_some_function.pack(side=tk.LEFT, padx=10)
+
+btn_close = ttk.Button(frame_buttons, text="Закрыть", command=stop)
+btn_close.pack(side=tk.LEFT, padx=10)
+
+fig, (ax1, ax2,ax3,ax4) = plt.subplots(nrows=4, ncols=1, figsize=(7, 5))
+lines1, = ax1.plot([], [], lw=2)
+lines2, = ax2.plot([], [], lw=2)
+lines3, = ax3.plot([], [], lw=2)
+lines4, = ax4.plot([], [], lw=2)
+ax1.set_title("21 место")
+ax2.set_title(" 22 место")
+ax3.set_title("21 место")
+ax4.set_title(" 22 место")
 
 
-def main():
-    root = tk.Tk()
-    root.title("Real-time Plot")
+canvas = FigureCanvasTkAgg(fig, root)
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-    frame = ttk.Frame(root, padding="10")
-    frame.grid(row=0, column=0)
+x, y1, y2, y3,y4 = [], [], [],[],[]
+counter = 0
+stop_graph = False
 
-    fig = Figure(figsize=(6, 4), dpi=100)
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_ylim(0, 1)
-    ax.set_title("Real-time Data")
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Value")
-
-    line, = ax.plot(np.zeros(100))
-
-    canvas = FigureCanvasTkAgg(fig, master=frame)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=0, column=0)
-
-    button_execute = ttk.Button(frame, text="Execute Function", command=on_button_click)
-    button_execute.grid(row=1, column=0)
-
-    button_quit = ttk.Button(frame, text="Close", command=lambda: close(root))
-    button_quit.grid(row=2, column=0)
-
-    update_thread = threading.Thread(target=update_plot, args=(line, canvas))
-    update_thread.start()
-
-    root.mainloop()
-
-
-if __name__ == "__main__":
-    main()
+root.after(50, update_graphs)
+root.mainloop()

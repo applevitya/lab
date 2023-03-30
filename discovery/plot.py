bceec5251cc8e5d4324d3b20be219f3e0ad3d264
applevitya <apple.vitya@mail.ru>
2023-03-30 20:44:15 +0300

    new

--- discovery/plot.py
+++ discovery/plot.py
@@ -1,5 +1,5 @@
 from ctypes import *
-#from  SDK import staticIO, device, dynamic_digital, dynamic_analog
+from  SDK import staticIO, device, dynamic_digital, dynamic_analog
 import sys
 import numpy as np
 import tkinter as tk
@@ -11,7 +11,6 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from matplotlib.figure import Figure
 
-"""
 if sys.platform.startswith("win"):
     dwf = cdll.dwf
 elif sys.platform.startswith("darwin"):
@@ -19,42 +18,32 @@ elif sys.platform.startswith("darwin"):
 else:
     dwf = cdll.LoadLibrary("libdwf.so")
 
-
-
-#### Открываем устройство  #######  
 hdwf = c_int()
 hdwf = device.open()
 
 
+led_off = [0 for i in range(64)]
+led_on = [1 for i in range(64)]
+dynamic_digital.led_matrix(hdwf, 6, 7, 5, led_off) # выключаем светодиоды
 
-##### задаем положительное напряжение #############
-
-#channel 0,1,2,3,4
-for i in range(5):
-    staticIO.turn_on_channel(hdwf, i)
-
-
-### led pins
-l_1 = 5 #data pin led
-l_2 = 6 # shift pin led
-l_3 = 5 # clock pin led
-
-### struct pins
-s_1 =1 # shift
-s_2 =1 # clock 
-s_3 =1 # data
-
-"""
+struct_index = [0 for i in range(64)]
+struct_index[20] = 1
+dynamic_analog.struct_measure(hdwf, 10, 9, 8, struct_index) # struct_measure(device_handle,shift,clock,data,matrix)
 
 def measure():
-    return np.random.randint(0, 10) #dynamic_analog.measure(hdwf,1)
-
+    values = []
+    for i in range(100):
+        values.append(dynamic_analog.measure(hdwf, 1))
+    return sum(values) / len(values)
 
 def some_function():
-    
+    dynamic_digital.led_matrix(hdwf, 6, 7, 5, led_on)
+    time.sleep(0.5)
+    dynamic_digital.led_matrix(hdwf, 6, 7, 5, led_off)
     print("Выполняется функция, не связанная с графиком")
 
 def close():
+    device.close(hdwf)
     print("Закрытие программы")
 
 def update_graphs():
@@ -84,7 +73,7 @@ def update_graphs():
 
         canvas.draw()
 
-        root.after(5, update_graphs)
+        root.after(50, update_graphs)
 
 def stop():
     global stop_graph
@@ -94,7 +83,7 @@ def stop():
 
 root = tk.Tk()
 root.title("Tkinter и Matplotlib")
-root.geometry("1000x1000")
+root.geometry("1000x600")
 
 frame_buttons = ttk.Frame(root)
 frame_buttons.pack(side=tk.TOP, pady=20)
@@ -105,23 +94,23 @@ btn_some_function.pack(side=tk.LEFT, padx=10)
 btn_close = ttk.Button(frame_buttons, text="Закрыть", command=stop)
 btn_close.pack(side=tk.LEFT, padx=10)
 
-fig, (ax1, ax2,ax3,ax4) = plt.subplots(nrows=4, ncols=1, figsize=(7, 50))
+fig, (ax1, ax2,ax3,ax4) = plt.subplots(nrows=4, ncols=1, figsize=(7, 5))
 lines1, = ax1.plot([], [], lw=2)
 lines2, = ax2.plot([], [], lw=2)
 lines3, = ax3.plot([], [], lw=2)
 lines4, = ax4.plot([], [], lw=2)
 ax1.set_title("21 место")
 ax2.set_title(" 22 место")
-ax3.set_title("23 место")
-ax4.set_title(" 24 место")
+ax3.set_title("21 место")
+ax4.set_title(" 22 место")
 
 
 canvas = FigureCanvasTkAgg(fig, root)
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=False)
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 x, y1, y2, y3,y4 = [], [], [],[],[]
 counter = 0
 stop_graph = False
 
-root.after(5, update_graphs)
+root.after(50, update_graphs)
 root.mainloop()

001410af3ae4095c5bae04367b38f036d7ba9715
applevitya <apple.vitya@mail.ru>
2023-08-22 14:24:35 +0300

    new

--- discovery/experiment.py
+++ discovery/experiment.py
@@ -2,12 +2,9 @@ from ctypes import *
 from  SDK import staticIO, device, dynamic_digital, dynamic_analog
 import sys
 import numpy as np
-import tkinter as tk
-from tkinter import ttk
-import random
+
 import time
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 from matplotlib.figure import Figure
 
@@ -21,12 +18,34 @@ led_off = [0 for i in range(64)]
 led_on = [1 for i in range(64)]
 dynamic_digital.led_matrix(hdwf, 6, 7, 5, led_off) # выключаем светодиоды
 
+iteration = 300
+
+def measure(index):
+    struct_index = [0 for i in range(64)]
+    struct_index[index-1] = 1
+    dynamic_analog.struct_measure(hdwf, 10, 9, 8, struct_index) # struct_measure(device_handle,shift,clock,data,matrix)
+    values = []
+    for i in range(iteration):
+        values.append(dynamic_analog.measure(hdwf, 1))
+    return sum(values) / len(values)
+
+def measure_2(index):
+    struct_index = [0 for i in range(64)]
+    struct_index[index-1] = 1
+    dynamic_analog.struct_measure(hdwf, 10, 9, 8, struct_index) # struct_measure(device_handle,shift,clock,data,matrix)
+    values = []
+    for i in range(iteration):
+        values.append(dynamic_analog.measure(hdwf, 2))
+    return sum(values) / len(values)
+
+
+
 ############################################################################################################################################################
 #calibration procedure
 
 
 
-known_indices = [1, 5, 10, 15]  # Заранее известные индексы
+known_indices = [22, 23, 24, 30]  # Заранее известные индексы
 initial_weights = []
 final_weights = []
 
@@ -48,6 +67,9 @@ for index in known_indices:
 
 print(final_weights)
 
+data=np.vstack((initial_weights,final_weights))
 # Запись массивов в файл
 with open("calibration.txt", "w") as file:
     np.savetxt(file, data)
+
+device.close(hdwf)

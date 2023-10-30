aec43b4668eb9a38fde8fd0a26117d4fd8c81548
applevitya <apple.vitya@mail.ru>
2023-10-30 14:32:32 +0300

    запуск эксперимента

--- discovery/experiment/setup_1.py
+++ discovery/experiment/setup_1.py
@@ -38,7 +38,52 @@ def measure_2(index):
         values.append(dynamic_analog.measure(hdwf, 2))
     return sum(values) / len(values)
 
+def weight_setting(arr):
+    value = []
+    for i, val in enumerate(arr):
+        if val != 0:
+            if i<32:
+                value.append(measure(i+1))
+            else:
+                value.append(measure_2(i+1))
+        else:
+            value.append(0)
+            
+    while any(abs(a - b) > 0.02 for a, b in zip(arr, value)):
+        led = np.array([int((a - b) > 0.02) for a, b in zip(arr, value)])
+        led = led.reshape((8,8))
+        led = np.flip(led,axis=1)
+        led = led.reshape((64,))
+        print(led)
+        dynamic_digital.led_matrix(hdwf, 6, 7, 5, list(led))
+        time.sleep(0.1)
+        dynamic_digital.led_matrix(hdwf, 6, 7, 5, led_off)
+
+        for i, val in enumerate(arr):
+            if val != 0:
+                if i<32:
+                    value[i] = measure(i+1)
+                else:
+                    value[i] = measure_2(i+1)
+    return value
+
 ####################################################################################################################################
 
+## зададим картинку
+
+pic = np.loadtxt('start.txt')
+pic = pic*2.2987315479909154/255
+pic = pic.reshape(-1)
+
+indexes = list(range(19, 25)) + list(range(27, 33)) + [34] + list(range(36, 41)) + list(range(43, 49)) + list(range(49, 50))
+weights = [0] * 64
+
+for i, index in enumerate(indexes):
+    weights[index-1] = pic[i]
+
+
+weight_setting(weights) # задали картинку 
+
+####### 
 
 
